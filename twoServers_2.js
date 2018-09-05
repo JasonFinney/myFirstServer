e0c9f5e1ed1ede22015961239b30f2e83dd366fe
Jason Finney <finney.jason.218@gmail.com>
2018-09-05 14:15:20 -0500

    it works!

--- twoServers_2.js
+++ twoServers_2.js
@@ -3,15 +3,12 @@ var http = require("http");
 var PORT1 = 7000;
 var PORT2 = 7500;
 
-function Request(request1, response) {
-    response.end("It Works!! Path Hit: " + request1.url + "  Everything you touch turns to gold!");
-};
-function handleRequest(request2, response) {
-    response.end("It doesn't work!! Path Hit: " + request2.url + "  You are only a failure at the important things...");
-};
-
-const server1 = http.createServer(Request);
-const server2 = http.createServer(handleRequest);
+const server1 = http.createServer(function (request, response) {
+    response.end("It Works!! Path Hit: " + request.url + "  Everything you touch turns to gold!");
+});
+const server2 = http.createServer(function (request, response) {
+    response.end("It Works!! Path Hit: " + request.url + "  You are only a failure at the important things...");
+});
 
 server1.listen(PORT1, function () {
     console.log("Server1 listening on: http://localhost:" + PORT1);
